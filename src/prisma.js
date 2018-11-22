bac84b1054122a0bdd08cc329bc1964637b08cb9
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-22 03:19:10 -0500

    Add Prisma into GraphQL Queries

--- src/prisma.js
+++ src/prisma.js
@@ -1,43 +1,43 @@
 import { Prisma } from 'prisma-binding';
 
-const prisma = new Prisma({
+export default new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466'
 });
 
-const createPostForUser = async (authorId, data) => {
+// const createPostForUser = async (authorId, data) => {
 
-    const userExists = await prisma.exists.User({ id: authorId });
+//     const userExists = await prisma.exists.User({ id: authorId });
 
-    if (!userExists) {
-        throw new Error('User not found.');
-    }
+//     if (!userExists) {
+//         throw new Error('User not found.');
+//     }
 
-    const post = await prisma.mutation.createPost({ 
-        data: {
-            ...data,
-            author: {
-                connect: {
-                    id: authorId
-                }
-            }
-        }
-    }, '{ author { id name posts { id title } } }');
+//     const post = await prisma.mutation.createPost({ 
+//         data: {
+//             ...data,
+//             author: {
+//                 connect: {
+//                     id: authorId
+//                 }
+//             }
+//         }
+//     }, '{ author { id name posts { id title } } }');
 
-    return post;
-};
+//     return post;
+// };
 
-const updatePostForUser = async (postId, data) => {
-    const postExists = await prisma.exists.Post({ id: postId });
+// const updatePostForUser = async (postId, data) => {
+//     const postExists = await prisma.exists.Post({ id: postId });
 
-    if (!postExists) {
-        throw new Error('Post not found.');
-    }
+//     if (!postExists) {
+//         throw new Error('Post not found.');
+//     }
 
-    const post = await prisma.mutation.updatePost({ 
-        data,
-        where: { id: postId }
-    }, '{ author { id name posts { id title } } }');
+//     const post = await prisma.mutation.updatePost({ 
+//         data,
+//         where: { id: postId }
+//     }, '{ author { id name posts { id title } } }');
 
-    return post;
-};
+//     return post;
+// };
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,25 +1,30 @@
 export default {
-    users(parent, args, { db }, info) {
-        if (!args.query) {
-            return db.users;
-        }
+    users(parent, args, { prisma }, info) {
 
-        return db.users.filter((user) => {
-            return user.name.toLowerCase().includes(args.query.toLowerCase());
-        })
-    },
-    posts(parent, args, { db }, info) {
-        if (!args.query) {
-            return db.posts;
-        }
+        return prisma.query.users(null, info);
+        // if (!args.query) {
+        //     return db.users;
+        // }
 
-        return db.posts.filter((post) => {
-            const titleMatch = post.title.toLowerCase().includes(args.query.toLowerCase());
-            const bodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
-            return titleMatch || bodyMatch;
-        })
+        // return db.users.filter((user) => {
+        //     return user.name.toLowerCase().includes(args.query.toLowerCase());
+        // })
     },
-    comments(parent, args, { db }, info) {
+    posts(parent, args, { prisma }, info) {
+
+        return prisma.query.posts(null, info);
+
+        // if (!args.query) {
+        //     return db.posts;
+        // }
+
+        // return db.posts.filter((post) => {
+        //     const titleMatch = post.title.toLowerCase().includes(args.query.toLowerCase());
+        //     const bodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
+        //     return titleMatch || bodyMatch;
+        // })
+    },
+    comments(parent, args, { db, prisma }, info) {
         return db.comments;
     },
     me() {
