116dde963782888cd9cbc4db8711d39f32890a88
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-23 04:03:39 -0500

    Close Prism to outside world

--- src/prisma.js
+++ src/prisma.js
@@ -2,42 +2,6 @@ import { Prisma } from 'prisma-binding';
 
 export default new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'http://localhost:4466'
+    endpoint: 'http://localhost:4466',
+    secret: 'supersecrettext'
 });
-
-// const createPostForUser = async (authorId, data) => {
-
-//     const userExists = await prisma.exists.User({ id: authorId });
-
-//     if (!userExists) {
-//         throw new Error('User not found.');
-//     }
-
-//     const post = await prisma.mutation.createPost({ 
-//         data: {
-//             ...data,
-//             author: {
-//                 connect: {
-//                     id: authorId
-//                 }
-//             }
-//         }
-//     }, '{ author { id name posts { id title } } }');
-
-//     return post;
-// };
-
-// const updatePostForUser = async (postId, data) => {
-//     const postExists = await prisma.exists.Post({ id: postId });
-
-//     if (!postExists) {
-//         throw new Error('Post not found.');
-//     }
-
-//     const post = await prisma.mutation.updatePost({ 
-//         data,
-//         where: { id: postId }
-//     }, '{ author { id name posts { id title } } }');
-
-//     return post;
-// };
