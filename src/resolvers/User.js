0abbe6734f2742a1575ca94c7fa9ea35a8cae85c
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-24 02:30:51 -0500

    Lock down email type on User

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,3 +1,13 @@
-export default {
+import getUserId from '../utils/getUserId';
 
+export default {
+    email(parent, args, { request }, info) {
+        const userId = getUserId(request, false);
+
+        if (userId && userId === parent.id) {
+            return parent.email;
+        } else {
+            return null;
+        }
+    }
 };
