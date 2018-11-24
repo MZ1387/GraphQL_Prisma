23e30f1e9a5f8083e1eb5b5d6e67ae1b8c722698
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-24 03:14:23 -0500

    Add fragments

--- src/prisma.js
+++ src/prisma.js
@@ -1,7 +1,9 @@
 import { Prisma } from 'prisma-binding';
+import { fragmentReplacements } from './resolvers';
 
 export default new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466',
-    secret: 'supersecrettext'
+    secret: 'supersecrettext',
+    fragmentReplacements 
 });
