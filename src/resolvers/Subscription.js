02557bd0319e6bb4a6ecd55b742239a1bd26dd51
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-11-24 04:33:41 -0500

    Lock down subscriptions

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,3 +1,5 @@
+import getUserId from '../utils/getUserId';
+
 export default {
     comment: {
         subscribe(parent, { postId }, { prisma }, info) {
@@ -18,6 +20,21 @@ export default {
                 where: {
                     node: {
                         published: true
+                    }
+                }
+            }, info);
+        }
+    },
+    myPost: {
+        subscribe(parent, args, { prisma, request }, info) {
+            const userId = getUserId(request);
+
+            return prisma.subscription.post({
+                where: {
+                    node: {
+                        author: {
+                            id: userId
+                        }
                     }
                 }
             }, info);
